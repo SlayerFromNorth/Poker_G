a1174811ad5ff70239fa588f686dd3ab594e23cc
SlayerFromNorth <mariusoa@gmail.com>
2020-12-27 22:30:58 +0100

    Update ai_poker_v01.py

--- ai_poker_v01.py
+++ ai_poker_v01.py
@@ -3,40 +3,54 @@ import numpy as np
 import random
 
 
-def ai_starter(kortene, status):
-    kortet = kortene
-    en_kortstokk = [a for a in range(51)]
-    [en_kortstokk.remove(b) for b in kortet]
-    kortet = []
-    for a in range(5-len(kortet)):
-        kort = random.choice(en_kortstokk)
-        en_kortstokk.remove(kort)
-        kortet.append(kort)
+def chance_ran (my_hand, board, epoker = 1000):
+    kortet = my_hand
+    bordet = board
+    ai_counter, Even_counter = 0, 0
 
-    a = engine_poker.find_Gant(kortet)
+    for _ in range(epoker):
+        kortene = [kortet[0], kortet[1]]
+        kortene += bordet
+        en_kortstokk = [a for a in range(52)]
+        [en_kortstokk.remove(b) for b in kortene]
+        for a in range(9 - len(kortene)):
+            kort = random.choice(en_kortstokk)
+            en_kortstokk.remove(kort)
+            kortene.append(kort)
 
-    inputs = [a[0], a[1]/1000000000]
-    weights = [-0.9, -1.5], [1.1, 0.2], [1.6, 0.7]
-    biases = [3.7, 2.8, 0.1]
+        ai_styrke = engine_poker.find_Gant(kortene[:-2])
+        Even_styrke = engine_poker.find_Gant(kortene[2:])
+
+        if ai_styrke > Even_styrke:
+            ai_counter += 1
+        else:
+            Even_counter += 1
+
+    return ai_counter/epoker
 
 
-    layer_outputs=[]
 
 
-    for neuron_weights, neuron_bias in zip(weights, biases):
-        # Zeroed output of given neuron
-        neuron_output = 0
-        # For each input and weight to the neuron
-        for n_input, weight in zip(inputs, neuron_weights):
-            # Multiply this input by associated weight
-            # and add to the neuron’s output variable
-            neuron_output += n_input*weight
-            # Add bias
-        neuron_output += neuron_bias
-        # Put neuron’s result to the layer’s output list
-        layer_outputs.append(neuron_output)
-    #print(layer_outputs)
-    gg = np.argmax(layer_outputs)
-    if gg == 0: return -1
-    if gg == 1: return status[2]-status[3]
-    if gg == 2: return status[1]
+def ai_starter(kortene, status=0):
+# Kortene, BS-Check, BS-Call, BS-Raise, AGV, CB
+
+    bs_check, bs_call, bs_raise = 0, 0, 0
+
+    myhand = kortene[0:2]
+    board = kortene[2:]
+    a = chance_ran(myhand, board)
+    inputs = [a, bs_check, bs_call, bs_raise]
+    # Fold - Call - Raise
+    weights = [[-1, 0.45, 1], [0, 0.04, 0.08], [0.03, 0.02, 0], [0.09, 0.04, 0]]
+    biases = [1, 0.3, 0]
+    lag = np.dot(inputs, weights) + biases
+    print(a)
+    print(lag)
+    return lag
+
+
+"""for a in [4,6,8,13,32,34,51,23,24,41,42]:
+    for b in [2,5,9,21,44,45,50]:
+        print(engine_poker.poker_names([a,b]))
+        ai_starter([a,b])
+"""
