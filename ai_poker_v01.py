ceda9d70438283de2d9651a71ea0443641f12203
SlayerFromNorth <mariusoa@gmail.com>
2021-03-01 11:10:28 +0100

    Update ai_poker_v01.py

--- ai_poker_v01.py
+++ ai_poker_v01.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 
 
-def chance_ran (my_hand, board, epoker = 1000):
+def chance_ran (my_hand, board, epoker=1000):
     kortet = my_hand
     bordet = board
     ai_counter, Even_counter = 0, 0
@@ -21,7 +21,7 @@ def chance_ran (my_hand, board, epoker = 1000):
         ai_styrke = engine_poker.find_Gant(kortene[:-2])
         Even_styrke = engine_poker.find_Gant(kortene[2:])
 
-        if ai_styrke > Even_styrke:
+        if ai_styrke >= Even_styrke:
             ai_counter += 1
         else:
             Even_counter += 1
@@ -31,22 +31,28 @@ def chance_ran (my_hand, board, epoker = 1000):
 
 
 
-def ai_starter(kortene, status=0):
-# Kortene, BS-Check, BS-Call, BS-Raise, AGV, CB
+def ai_starter(kortene, status=[0,0,0,0,0,0]):
+# Kortene,POT, CAll_sum, AGV, BS-Check, BS-Call, BS-Raise
+    print(status)
+    bs_check, bs_call, bs_raise = status[3], status[4], status[5]
+    if (status[1] / status[0]) < 0.5:
+        CB = (status[1]/status[0])
+    else:
+        CB = 2.5 * (status[1]/status[0])
 
-    bs_check, bs_call, bs_raise = 0, 0, 0
-
+    print("-----CB-----", CB)
     myhand = kortene[0:2]
     board = kortene[2:]
     a = chance_ran(myhand, board)
-    inputs = [a, bs_check, bs_call, bs_raise]
+    inputs = [a, bs_check, bs_call, bs_raise, CB]
     # Fold - Call - Raise
-    weights = [[-1, 0.45, 1], [0, 0.04, 0.08], [0.03, 0.02, 0], [0.09, 0.04, 0]]
-    biases = [1, 0.3, 0]
+    weights = [[-1, 0.55, 1], [0, 0.07, 0.15], [0.11, 0.05, 0], [0.18, 0.13, 0], [0.3, 0.1, 0]]
+    biases = [1, 0.35, 0.1]
     lag = np.dot(inputs, weights) + biases
-    print(a)
-    print(lag)
-    return lag
+    print("vinner SJANSE:", a)
+    out = np.argmax(lag)
+
+    return [out, lag]
 
 
 """for a in [4,6,8,13,32,34,51,23,24,41,42]:
